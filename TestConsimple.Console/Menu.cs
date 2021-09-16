6b736fdc837fe9487977c724a9518934a1930eb1
Sobol <laslosobol@gmail.com>
2021-09-16 17:18:38 +0300

    upd: command interface

---
+++ TestConsimple.Console/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using TestConsimple.Logic;
+
+namespace TestConsimple.Console
+{
+    public static class Menu
+    {
+        public static void Start()
+        {
+            var menu = 0;
+            System.Console.Write("Choose action:\n1.Send Request\n2.Exit");
+            menu = Convert.ToInt32(System.Console.ReadLine());
+            if (menu == 1)
+            {
+                var root = Deserializer.Deserialize(JsonReader.Connection("https://tester.consimple.pro/"));
+                    
+                System.Console.WriteLine("Product Name      Category Name");
+                foreach (var product in root.Products)
+                {
+                    System.Console.WriteLine
+                        ($"{product.Name}       {root.Categories.Where(_ => _.Id == product.CategoryId).Select(_ => _.Name).FirstOrDefault()}");
+                }
+                Start();
+            }
+        }
+    }
+}
--- TestConsimple.Console/Program.cs
+++ TestConsimple.Console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Text.Json;
-using TestConsimple.Logic;
-using System.Linq;
 
 namespace TestConsimple.Console
 {
@@ -10,13 +6,7 @@ namespace TestConsimple.Console
     {
         static void Main(string[] args)
         {
-            var root = Deserializer.Deserialize(JsonReader.Connection("https://tester.consimple.pro/"));
-            System.Console.WriteLine("Product Name      Category Name");
-            foreach (var product in root.Products)
-            {
-                System.Console.WriteLine
-                    ($"{product.Name}       {root.Categories.Where(_ => _.Id == product.CategoryId).Select(_ => _.Name).FirstOrDefault()}");
-            }
+            Menu.Start();
         }
     }
 }
